dbcedf9ca70d11ddd0027e95edd421a208e065bf
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2026-05-20 01:00:23 +0800

    Configure live Render API URL and build configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend-honoridez.onrender.com',
+    apiUrl: 'https://fullstack-backend-honoridez.onrender.com',
     useFakeBackend: false
 };
